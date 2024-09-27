8838da590c7b7c28aa5ff2a22753ae19ea08acbc
James Tan <jamestyj@gmail.com>
2024-09-27 22:57:23 +0800

    Update HealthHub.sg.js

--- HealthHub.sg.js
+++ HealthHub.sg.js
@@ -16,19 +16,21 @@
 
     setInterval(function() {
         for (const className of [
+            "dashboard__nav__bottom__button",
+            "global-footer page-holder",
+            "breadcrumbs-area",
             "global-logo-bar",
             "top-holder",
             "add-tab-nav",
             "dashboard__nav__action col",
             "card__buttons",
             "card__button",
-            "card__bottom"
+            "card__bottom",
+            "card__top__cal-add",
+            "btn-back-to-top"
         ]) {
             for (const e of document.getElementsByClassName(className)) {
-                if (e) {
-                    e.remove();
-                    console.log(e);
-                }
+                if (e) e.remove();
             }
         }
     }, 1000);
